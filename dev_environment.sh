f5240a3b4710d7ab08499b19e7da93202b9d627e
Eric Meinhardt <ericmeinhardt@gmail.com>
2019-05-26 16:58:13 -0700

    Added sparse to dev_environment shell script

--- dev_environment.sh
+++ dev_environment.sh
@@ -4,5 +4,6 @@ conda create --name anvil python=3.6 csvkit jupyter scipy matplotlib numpy tqdm 
 conda activate anvil
 conda install -c conda-forge jupyter_contrib_nbextensions papermill plotnine panel bokeh holoviews hvplot param datashader funcy dask-jobqueue
 pip install more_itertools
+pip install sparse
 conda install pytorch torchvision cudatoolkit=9.2 -c pytorch
 
