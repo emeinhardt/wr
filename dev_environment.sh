d1adac5231f007bd4fbe994b6ad3f65fb08bb08d
emeinhardt <ericmeinhardt@gmail.com>
2020-07-06 13:32:53 -0700

    Update dev_environment.sh

--- dev_environment.sh
+++ dev_environment.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-conda create --name anvil python=3.6 csvkit jupyter scipy matplotlib numpy tqdm joblib numba cudatoolkit=9.2 pyculib 
-conda activate anvil
-conda install -c conda-forge jupyter_contrib_nbextensions papermill plotnine panel bokeh holoviews hvplot param datashader funcy dask-jobqueue
-pip install more_itertools
-pip install sparse
-conda install pytorch torchvision cudatoolkit=9.2 -c pytorch
+conda create --name jax-gpu2 python=3.7 jupyter scipy matplotlib numpy tqdm joblib numba cudatoolkit=10.0 cupy dask nose tensorflow-gpu h5py conda-build pytest
 
+conda install -c conda-forge jupyter_contrib_nbextensions papermill plotnine panel bokeh holoviews hvplot param datashader funcy dask-jobqueue tiledb-py watermark opt_einsum
+
+pip install cityhash
+
+conda install -c conda-forge distributed=2.3 cloudpickle fastparquet fsspec murmurhash partd pyarrow toolz xxhash
+
+pip install nbimporter more_itertools sparse
+
+conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
